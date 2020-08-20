276168312e3f39f197a70c306e46fecf49fc6c87
Ramvashakan <vramvasu99@gmail.com>
2020-08-20 20:36:34 +0530

    team sector ui design

--- js/script.js
+++ js/script.js
@@ -10,3 +10,25 @@ $(window).on('load', () => {  //website to load fully
     $('#preloader').delay(350).fadeOut('slow'); //white backgroung will be faded
 
 });
+
+/*============================================= 
+                Team
+================================================*/
+
+$(()=>{
+
+    $("#team-members").owlCarousel({
+
+        items:2,
+        autoplay: true,
+        smartSpeed:700,
+        loop: true,
+        autoplayHoverPause:true,
+        nav: true,
+        dots: false,
+        navText: ['<i class = "fa fa-angle-left left"></i>','<i class="fa fa-angle-right left"></i>']
+
+    });
+
+});
+
