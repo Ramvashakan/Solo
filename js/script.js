6e4953f77a21e1c315ca23500a8a8f0fe5e8cbe5
Ramvashakan <vramvasu99@gmail.com>
2020-08-20 12:41:54 +0530

    Solo website with home section and about section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+/*============================================= 
+                PreLoader
+================================================*/
+
+$(window).on('load', () => {  //website to load fully
+
+    $('#status').fadeOut();    //gif will be faded
+
+    $('#preloader').delay(350).fadeOut('slow'); //white backgroung will be faded
+
+});
