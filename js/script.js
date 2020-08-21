61c1a4b3ace3dc9e8f792da58812ac426ad5ab98
Ramvashakan <vramvasu99@gmail.com>
2020-08-21 23:10:43 +0530

    work UI over

--- js/script.js
+++ js/script.js
@@ -74,3 +74,76 @@ $(function () {
     })
 
 });
+
+
+
+/*============================================= 
+                PortFolio 
+================================================*/
+
+
+$(window).on('load', function () {
+
+    $("#isotope-container").isotope({});
+
+    $("#isotope-filters").on('click', 'button', function () {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $("#isotope-container").isotope({
+
+            filter: filterValue
+
+        });
+
+        $("#isotope-filters").find('.active').removeClass('active');
+        $(this).addClass('active');
+
+    });
+
+});
+
+
+/*============================================= 
+                Magnifier
+================================================*/
+
+$(function () {
+
+    $("#portfolio-wrapper").magnificPopup({
+
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+
+            enabled: true
+
+        }
+
+    })
+
+
+});
+
+
+/*============================================= 
+                Testimonial
+================================================*/
+
+
+$(function () {
+
+    $("#testimonial-slider").owlCarousel({
+
+        items: 1,
+        autoplay: false,
+        smartSpeed: 500,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class = "fa fa-angle-left"></i>', '<i class="fa fa-angle-right "></i>']
+
+    });
+
+});
